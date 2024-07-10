db6990c1ea103dec98286dc706df55e36eed920f
yunioseijo <yunioseijo@gmail.com>
2024-07-10 22:14:58 +0200

    funcionan las rutas con iconos

--- src/app/dashboard/pages/dashboard/dashboard.component.ts
+++ src/app/dashboard/pages/dashboard/dashboard.component.ts
@@ -1,17 +1,76 @@
 import { Component, OnInit } from '@angular/core';
 import { RouterLink, RouterLinkActive, RouterOutlet } from '@angular/router';
 import { AvatarComponent } from "../../../components/avatar/avatar.component";
+import { PrimeNgModule } from '../../../prime-ng/prime-ng.module';
+import { MenuItem } from 'primeng/api';
 
 @Component({
   selector: 'app-dashboard',
   standalone: true,
-  imports: [RouterOutlet, RouterLink, RouterLinkActive, AvatarComponent],
+  imports: [RouterOutlet, RouterLink, RouterLinkActive, AvatarComponent, PrimeNgModule],
   templateUrl: './dashboard.component.html',
   styleUrl: './dashboard.component.css'
 })
 export class DashboardComponent implements OnInit {
-  ngOnInit(): void {
-    console.log('on it DashboardComponent');
-  }
+  items: MenuItem[] | undefined;
+  ngOnInit() {
+    this.items = [
+        {
+            label: 'Dashboard',
+            icon: 'pi pi-home'
+        },
+        {
+            label: 'Computers',
+            // icon: 'fa-fw fas fa-laptop',
+            icon: 'pi pi-desktop',
+            routerLink: 'computers'
+        },
+        {
+          label: 'Users',
+          icon: 'pi pi-users',
+          routerLink: 'users'
+        },
+        {
+            label: 'Projects',
+            icon: 'pi pi-search',
+            items: [
+                {
+                    label: 'Core',
+                    icon: 'pi pi-bolt',
+                    shortcut: '⌘+S'
+                },
+                {
+                    label: 'Blocks',
+                    icon: 'pi pi-server',
+                    shortcut: '⌘+B'
+                },
+                {
+                    label: 'UI Kit',
+                    icon: 'pi pi-pencil',
+                    shortcut: '⌘+U'
+                },
+                {
+                    separator: true
+                },
+                {
+                    label: 'Templates',
+                    icon: 'pi pi-palette',
+                    items: [
+                        {
+                            label: 'Apollo',
+                            icon: 'pi pi-palette',
+                            badge: '2'
+                        },
+                        {
+                            label: 'Ultima',
+                            icon: 'pi pi-palette',
+                            badge: '3'
+                        }
+                    ]
+                }
+            ]
+        },
 
+    ];
+}
 }
